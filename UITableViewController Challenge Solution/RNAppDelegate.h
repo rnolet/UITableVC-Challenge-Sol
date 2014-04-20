b624fa58dc09101659328a0d0e1a3016c5e77fee
Richard Nolet <Richard.Nolet@gee.com>
2014-04-20 01:14:08 -0400

    Initial Commit

---
+++ UITableViewController Challenge Solution/RNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RNAppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Ricardo Nolet on 2014/04/20/.
+//  Copyright (c) 2014 Ricardo Nolet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewController Challenge Solution/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UITableViewController Challenge Solution
+//
+//  Created by Ricardo Nolet on 2014/04/20/.
+//  Copyright (c) 2014 Ricardo Nolet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RNAppDelegate class]));
+    }
+}
